cde296230e1374f14cf1e69d2cf5222643610edd
sebasArbelaez <sebastianarbelaez2314@gmail.com>
2021-03-30 20:58:32 -0500

    version 2

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>MesaAyuda</title>
+  <link rel="stylesheet" href="css/index.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" type="image/png" href="img/logo_size.jpg">
+</head>
+
+<body>
+  <header>
+      <?php include "header.html"?>
+  </header>
+
+  <main>
+    <section class="container-video">
+       <h1 class="titulo-video">Tutoria | como manejar la Mesa de Ayuda</h1>
+      <iframe class="iframe"  src="https://www.youtube.com/embed/Aqg3hZTpg1k?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer;
+      autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </section>
+  </main>
+
+  <footer>
+    <?php include "footer.html"  ?>
+  </footer>
+</body>
+</html>
---
+++ integrantes.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+  <meta charset="utf-8">
+  <title>MesaAyuda</title>
+  <link rel="stylesheet" href="css/index.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <header>
+      <?php include "header.html"?>
+  </header>
+
+  <main>
+    <!-- <div class="container-video">
+       <h1 class="titulo-video">Tutoria | como manejar la Mesa de Ayuda</h1>
+      <iframe class="iframe"  src="https://www.youtube.com/embed/Aqg3hZTpg1k?autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer;
+      autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <h1 class="descripcion-video">Aprende como maneja el aplicativo de "Mesa de Ayuda"</h1>
+    </div> -->
+  </main>
+
+  <footer>
+    <?php include "footer.html"  ?>
+  </footer>
+</body>
+</html>
